a08826c76eefc469e0f100f86c4f64b7b63f38fc
James <jamesandrewpike@gmail.com>
2025-01-09 15:59:43 -0500

    commit

--- src/components/widgets/Roadmap.tsx
+++ src/components/widgets/Roadmap.tsx
@@ -4,7 +4,7 @@ import { Image } from "@unpic/qwik";
 
 export default component$(() => {
   return (
-    <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 mb-2 rounded-md">
+    <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 mb-0 rounded-md">
     <section class="relative rounded-md not-prose mx-1 my-1 bg-white/75 dark:bg-gray-900/90">
 
     <Image
--- src/components/widgets/Tokenomics.tsx
+++ src/components/widgets/Tokenomics.tsx
@@ -5,7 +5,7 @@ import Stats from './Stats';
 
 export default component$(() => {
   return (
-    <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 mb-2 rounded-md">
+    <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 rounded-md">
     <section class="relative rounded-md not-prose mx-1 my-1 bg-white/75 dark:bg-gray-900/90">
 <Stats/>
     <Image
