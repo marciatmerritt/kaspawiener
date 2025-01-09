02fa0a3b1210bd6168e1b98442e75569f9a23b4c
James <jamesandrewpike@gmail.com>
2025-01-09 15:02:55 -0500

    commmit

--- src/routes/(roadmap)/roadmap/index.tsx
+++ src/routes/(roadmap)/roadmap/index.tsx
@@ -1,7 +1,6 @@
 import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import Roadmap from "~/components/widgets/Roadmap";
-import Tokenomics from "~/components/widgets/Tokenomics";
 
 export default component$(() => {
   return (
