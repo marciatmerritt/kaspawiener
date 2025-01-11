77ccaf276e4e28a2afff363df38123ba792ba913
James <jamesandrewpike@gmail.com>
2025-01-11 09:41:31 -0500

    commit

--- src/routes/(pricing)/pricing/index.tsx
+++ src/routes/(pricing)/pricing/index.tsx
@@ -1,24 +1,10 @@
 import { component$ } from "@builder.io/qwik";
 import Pricing from "~/components/widgets/Pricing";
 
-
-
-
-
 export default component$(() => {
   return (
     <>
-      <Pricing highlight="Pricing"
-        title="Flexible Pricing Options"
-        subtitle="Choose a one-time payment or pay it off over time."
-         items={[ {
-
-          },
-        ]}
-      />
-
-
-      
+   <Pricing/>
     </>
   );
 });
