32da976fb8a8095a7876d8e23861933ba02ef29f
James <jamesandrewpike@gmail.com>
2025-01-11 20:25:14 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -73,7 +73,48 @@ export default component$(() => {
 <Reviews/>
 <Pricing/>
 
-
+<FAQs
+        highlight="Features"
+        title="What you get with Qwind"
+        subtitle="Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque rem aperiam, eaque ipsa quae."
+        items={[
+          {
+            title: "Qwik + Tailwind CSS Integration",
+            description:
+              "A seamless integration between two great frameworks that offer high productivity, performance and versatility.",
+            icon: IconBrandTailwind,
+          },
+          {
+            title: "Ready-to-use Components",
+            description:
+              "Widgets made with Tailwind CSS ready to be used in Marketing Websites, SaaS, Blogs, Personal Profiles, Small Business...",
+            icon: IconApps,
+          },
+          {
+            title: "Best Practices",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+          },
+          {
+            title: "Excellent Page Speed",
+            description:
+              "Having a good page speed impacts organic search ranking, improves user experience (UI/UX) and increase conversion rates.",
+            icon: IconRocket,
+          },
+          {
+            title: "Search Engine Optimization (SEO)",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+            icon: IconBrandGoogle,
+          },
+          {
+            title: "Open to new ideas and contributions",
+            description:
+              "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi sagittis, quam nec venenatis lobortis, mi risus tempus nulla.",
+            icon: IconBulb
+          },
+        ]}
+      />
 
 <Contact/>
    
