7a337e2581d80f927c2e14f839a3baa9c74be387
James <jamesandrewpike@gmail.com>
2025-01-11 20:30:46 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -48,7 +48,8 @@ export default component$(() => {
 
 
           <div class="basis-1/2 ">
-
+          {/* <object type="image/svg+xml" class="border-radius-dot-25 rounded-md" data="https://cdn.svgator.com/images/2021/10/animated-astronaut-illustration.svg" width="100%"></object> */}
+          <img src="https://cdn.svgator.com/images/2021/10/solar-system-animation.svg" width="700" height="400" fetchPriority="high" alt="Animated Vectors Flying to the Moon and Back: About the Space Animation Trend"/>
           </div>
 
         </div>
