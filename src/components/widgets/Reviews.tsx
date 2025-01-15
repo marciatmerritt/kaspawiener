c09f6daff8ee23b95d7efc64d888f3b265e22966
James <jamesandrewpike@gmail.com>
2025-01-15 14:24:18 -0500

    commit

--- src/components/widgets/Reviews.tsx
+++ src/components/widgets/Reviews.tsx
@@ -66,8 +66,8 @@ export default component$(() => {
 
       <section class="bg-white dark:bg-gray-900">
         <div class="mx-auto max-w-7xl lg:py-16 lg:px-6">
-          <div class="py-3 px-3 mx-auto bg-gray-200 dark:bg-gray-800 lg:py-16 lg:px-6">
-            <div class="grid gap-8 lg:grid-cols-3">
+          <div class="py-4 px-3 mx-auto bg-gray-200 dark:bg-gray-800 lg:py-16 lg:px-6">
+            <div class="grid gap-4 md:gap-8 lg:grid-cols-3">
               {reviews.map(({ title, text, name, position, image}, index) => (
                 <figure
                   key={index}
