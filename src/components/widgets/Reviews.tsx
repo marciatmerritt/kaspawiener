a7ae586cdf3dc8398ccefc2dd9b013ebd8221ecc
James <jamesandrewpike@gmail.com>
2025-01-15 14:03:57 -0500

    commit

--- src/components/widgets/Reviews.tsx
+++ src/components/widgets/Reviews.tsx
@@ -2,6 +2,57 @@ import { component$ } from '@builder.io/qwik';
 import { Headline } from '../ui/Headline';
 import { PageWrapper } from '../common/PageWrapper';
 
+const reviews = [
+  {
+    title: 'Great insights and generous heart',
+    text: '"Thanks Dave Andrews for your great insights, talent and generous heart!"',
+    name: 'Carole Françoise Noel',
+    position: 'CTO at Open AI',
+    imgSrc: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png',
+    border: false,
+  },
+  {
+    title: 'Collaboration between artist and printer',
+    text: '"It\'s always a collaboration between photographer and printer, and he is an artist from whom I learn so very much!"',
+    name: 'Ruth Dick',
+    position: 'Software Engineer at Tesla',
+    imgSrc: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png',
+    border: true,
+  },
+  {
+    title: 'Always high-caliber work',
+    text: '"Dave is an artist in his own right who shares his knowledge with his clients willingly. His work is beautiful and always of the same high caliber. I only use Dave!"',
+    name: 'Joy Kardish',
+    position: 'CEO at Oracle',
+    imgSrc: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png',
+    border: true,
+  },
+  {
+    title: 'Attention to detail',
+    text: '"Dave is the best! Great results & attention to detail from start to finish."',
+    name: 'Andrew Balfour',
+    position: 'CTO at Open AI',
+    imgSrc: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png',
+    border: false,
+  },
+  {
+    title: 'Dedicated to the artists',
+    text: '"Dave invests himself in the work he prints for the artists he serves."',
+    name: 'Alan Mirabelli',
+    position: 'Software Engineer at Tesla',
+    imgSrc: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png',
+    border: false,
+  },
+  {
+    title: 'Craftsmanship and love',
+    text: '"Dave’s attention to detail and love of the craft is evident in his prints."',
+    name: 'Beth Gordon',
+    position: 'CEO at Oracle',
+    imgSrc: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png',
+    border: false,
+  },
+];
+
 export default component$(() => {
   return (
     <div>
@@ -15,106 +66,33 @@ export default component$(() => {
       </PageWrapper>
 
       <section class="bg-white dark:bg-gray-900">
-        <div class=" mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-
-
-          <div class="py-2 px-2 mx-auto bg-gray-200 dark:bg-gray-800  lg:py-16 lg:px-6">
-
-
-
-            <div class=" grid gap-8 lg:grid-cols-3">
-              <div class="space-y-3">
-
-                <div class="p-2 bg-gray-100">
-                  <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-                    <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-                      <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Great insights and generous heart</h3>
-                      <p class="my-4">"Thanks Dave Andrews for your great insights, talent and generous heart!"</p>
-                    </blockquote>
-                    <figcaption class="flex items-center space-x-3">
-                      <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="profile picture" />
-                      <div class="space-y-0.5 font-medium dark:text-white">
-                        <div>Carole Françoise Noel</div>
-                        <div class="text-sm font-light text-gray-500 dark:text-gray-400">CTO at Open AI</div>
+        <div class="mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+          <div class="py-2 px-2 mx-auto bg-gray-200 dark:bg-gray-800 lg:py-16 lg:px-6">
+            <div class="grid gap-8 lg:grid-cols-3">
+              {reviews.map((review, index) => (
+                <figure
+                  key={index}
+                  class={`p-6 bg-gray-50 rounded dark:bg-gray-800 ${review.border ? 'border-8 border-gray-100' : ''}`}
+                >
+                  <blockquote class="text-sm text-gray-500 dark:text-gray-400">
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">{review.title}</h3>
+                    <p class="my-4">{review.text}</p>
+                  </blockquote>
+                  <figcaption class="flex items-center space-x-3">
+                    <img
+                      class="w-9 h-9 rounded-full"
+                      src={review.imgSrc}
+                      alt="profile picture"
+                    />
+                    <div class="space-y-0.5 font-medium dark:text-white">
+                      <div>{review.name}</div>
+                      <div class="text-sm font-light text-gray-500 dark:text-gray-400">
+                        {review.position}
                       </div>
-                    </figcaption>
-                  </figure>
-                </div>
-
-                <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800 border-gray-100 border-8">
-                  <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Collaboration between artist and printer</h3>
-                    <p class="my-4">"It's always a collaboration between photographer and printer, and he is an artist from whom I learn so very much!"</p>
-                  </blockquote>
-                  <figcaption class="flex items-center space-x-3">
-                    <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture" />
-                    <div class="space-y-0.5 font-medium dark:text-white">
-                      <div>Ruth Dick</div>
-                      <div class="text-sm font-light text-gray-500 dark:text-gray-400">Software Engineer at Tesla</div>
                     </div>
                   </figcaption>
                 </figure>
-
-                <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800 border-8">
-                  <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Always high-caliber work</h3>
-                    <p class="my-4">"Dave is an artist in his own right who shares his knowledge with his clients willingly. His work is beautiful and always of the same high caliber. I only use Dave!"</p>
-                  </blockquote>
-                  <figcaption class="flex items-center space-x-3">
-                    <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture" />
-                    <div class="space-y-0.5 font-medium dark:text-white">
-                      <div>Joy Kardish</div>
-                      <div class="text-sm font-light text-gray-500 dark:text-gray-400">CEO at Oracle</div>
-                    </div>
-                  </figcaption>
-                </figure>
-
-                <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-                  <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Attention to detail</h3>
-                    <p class="my-4">"Dave is the best! Great results & attention to detail from start to finish."</p>
-                  </blockquote>
-                  <figcaption class="flex items-center space-x-3">
-                    <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/bonnie-green.png" alt="profile picture" />
-                    <div class="space-y-0.5 font-medium dark:text-white">
-                      <div>Andrew Balfour</div>
-                      <div class="text-sm font-light text-gray-500 dark:text-gray-400">CTO at Open AI</div>
-                    </div>
-                  </figcaption>
-                </figure>
-
-                <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-                  <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Dedicated to the artists</h3>
-                    <p class="my-4">"Dave invests himself in the work he prints for the artists he serves."</p>
-                  </blockquote>
-                  <figcaption class="flex items-center space-x-3">
-                    <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/karen-nelson.png" alt="profile picture" />
-                    <div class="space-y-0.5 font-medium dark:text-white">
-                      <div>Alan Mirabelli</div>
-                      <div class="text-sm font-light text-gray-500 dark:text-gray-400">Software Engineer at Tesla</div>
-                    </div>
-                  </figcaption>
-                </figure>
-
-                <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
-                  <blockquote class="text-sm text-gray-500 dark:text-gray-400">
-                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Craftsmanship and love</h3>
-                    <p class="my-4">"Dave’s attention to detail and love of the craft is evident in his prints."</p>
-                  </blockquote>
-                  <figcaption class="flex items-center space-x-3">
-                    <img class="w-9 h-9 rounded-full" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png" alt="profile picture" />
-                    <div class="space-y-0.5 font-medium dark:text-white">
-                      <div>Beth Gordon</div>
-                      <div class="text-sm font-light text-gray-500 dark:text-gray-400">CEO at Oracle</div>
-                    </div>
-                  </figcaption>
-                </figure>
-
-              </div>
-
-
-
+              ))}
             </div>
           </div>
         </div>
