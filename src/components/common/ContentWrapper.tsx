032eade78a0e50d97cfc246fd937a7fe795b0c90
James <jamesandrewpike@gmail.com>
2025-01-18 21:13:16 -0500

    commit

--- src/components/common/ContentWrapper.tsx
+++ src/components/common/ContentWrapper.tsx
@@ -1,16 +1,11 @@
-
-import { component$, Slot } from '@builder.io/qwik';
+import { component$, Slot } from "@builder.io/qwik";
 
 export const ContentWrapper = component$(() => {
   return (
-    <section class="bg-gray-50 dark:bg-gray-900">
-<div class=" mx-auto max-w-screen-xl px-2.5 py-3 lg:py-16 lg:px-6">
-          <Slot />
-          </div>
-          </section>
-     
-      
-
-    
+    <section class="bg-gray-50 dark:bg-gray-900 transition-colors duration-300">
+      <div class="mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8 py-6 sm:py-10 lg:py-16">
+        <Slot />
+      </div>
+    </section>
   );
 });
