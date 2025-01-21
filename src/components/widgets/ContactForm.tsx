265349b7c185549d8578241e3be093d342bb3981
James <jamesandrewpike@gmail.com>
2025-01-21 18:25:31 -0500

    commit

--- src/components/widgets/ContactForm.tsx
+++ src/components/widgets/ContactForm.tsx
@@ -72,6 +72,16 @@ export default component$(() => {
                         <input type="number" id="phone-number" class="block p-2.5 w-full text-md text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" placeholder="+12 345 6789" required/>
                         </div>
                     </div>
+                    <div>
+                        <label for="topic" class="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400">Topic</label>
+                        <select id="topic" class="bg-gray-50 border border-gray-300 text-gray-900 text-md rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                            <option selected>Select a topic</option>
+                            <option value="US">Switch plans and add-ons</option>
+                            <option value="CA">Billing & Invoice</option>
+                            <option value="FR">I can't log in to Flowbite</option>
+                            <option value="DE">Parental controls</option>
+                        </select>
+                    </div>
                     <div class="sm:col-span-2">
                         <label for="message" class="block text-sm/6 font-semibold text-gray-900 dark:text-white">Message</label>
                         <div class="mt-2.5">
