f718b02de588fe4e16daa06ad9dbc1aa855289d6
James <jamesandrewpike@gmail.com>
2025-01-21 15:45:35 -0500

    commit

--- src/components/pages/Services.tsx
+++ src/components/pages/Services.tsx
@@ -2,7 +2,6 @@ import { component$ } from '@builder.io/qwik';
 import { Headline } from '../ui/Headline';
 import { PageWrapper } from '../common/PageWrapper';
 import ServiceList from '../widgets/ServiceList';
-import { ContentWrapper } from '../common/ContentWrapper';
 
 export default component$(() => {
   return (
