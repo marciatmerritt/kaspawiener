a4ba3d91c0d30bcd8cef489cc363ab3deea2bebf
James <jamesandrewpike@gmail.com>
2025-01-21 11:07:04 -0500

    commit

--- src/components/widgets/ContactForm.tsx
+++ src/components/widgets/ContactForm.tsx
@@ -79,8 +79,8 @@ export default component$(() => {
                         </div>
                     </div>
                 </div>
-                <div class="mt-8 flex w-full">
-                <button type="submit" class="py-3 px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
+                <div class="mt-8 flex">
+                <button type="submit" class="py-3 w-full px-5 text-sm font-medium text-center text-white rounded-lg bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
                 </div>
             </div>
         </form>
