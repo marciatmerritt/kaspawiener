322662eff72a0ea1671138faf257d6109f0c1a95
James <jamesandrewpike@gmail.com>
2025-01-21 22:42:50 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -1,65 +1,79 @@
 import { component$ } from "@builder.io/qwik";
+import { Image } from "@unpic/qwik";
+
+
+const coverImage =
+  "/images/Print.mp4";
 
 export default component$(() => {
   return (
     <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 mb-2 border-radius-dot-25 rounded-lg">
 
-    <section class="relative rounded-lg  md:-mt-[76px] not-prose mx-1.5 my-1.5 bg-white/85 dark:bg-gray-900/85 border-radius-dot-25">
-      <div class="relative max-w-7xl mx-auto px-4 sm:px-10 ">
-        <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
-        <div class="pt-6 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
-          <div class="basis-1/2 text-center lg:text-left pb-8 md:pb-16 mx-auto">
-          <a href="/services" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
-          <span class="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">New</span> <span class="text-sm font-medium">25% Off Prints in January</span> 
-          <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
-      </a>
-            <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
-            
-              <span class="text-primary">Digital Art & Restoration</span>
-            </h1>
-            <div class="max-w-3xl mx-auto lg:max-w-none">
-              <p class="text-xl mb-6 dark:text-slate-300">
-              Bringing Your Memories to Life-- Expert Digital Art, Restoration, and Print Services
-              </p>
+      <section class="relative rounded-lg  md:-mt-[76px] not-prose mx-1.5 my-1.5 bg-white/85 dark:bg-gray-900/85 border-radius-dot-25">
+        <div class="relative max-w-7xl mx-auto px-4 sm:px-10 ">
+          <div class="pt-0 md:pt-[76px] pointer-events-none"></div>
+          <div class="pt-6 md:py-20 lg:py-0 lg:flex lg:items-center lg:h-screen lg:gap-8">
+            <div class="basis-1/2 text-center lg:text-left pb-8 md:pb-16 mx-auto">
+              <a href="/services" class="inline-flex justify-between items-center py-1 px-1 pr-4 mb-6 text-sm text-gray-700 bg-gray-100 rounded-full dark:bg-gray-800 dark:text-white hover:bg-gray-200 dark:hover:bg-gray-700" role="alert">
+                <span class="text-xs bg-primary rounded-full text-white px-4 py-1.5 mr-3">Sale</span> <span class="text-sm font-medium">Save 25% On All Prints</span>
+                <svg class="ml-2 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+              </a>
+              <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
 
-              <div class=" sm:max-w-md m-auto grid grid-cols-1 border-radius-dot-25 gap-4 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
-              <a href="/services">
-                <div class="flex w-full">
-                 
-                  <button class="btn w-full bg-primary-200 py-2 rounded-sm dark:bg-primary-200">
-                    Our Services
-                  </button>
-                  
+                <span class="text-primary">Bring Your Memories to Life</span>
+              </h1>
+              <div class="max-w-3xl mx-auto lg:max-w-none">
+                <p class="text-xl mb-6 dark:text-slate-300">
+                Expertly restoring photos, creating custom digital art, and delivering premium prints to preserve your most treasured moments.
+                </p>
+
+                <div class=" sm:max-w-md m-auto grid grid-cols-1 border-radius-dot-25 gap-4 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
+                  <a href="/services">
+                    <div class="flex w-full">
+
+                      <button class="btn w-full bg-primary-300 py-2 rounded-sm dark:bg-primary-400">
+                        Our Services
+                      </button>
+
+                    </div>
+                  </a>
+                  <a href="/contact">
+                    <div class="flex w-full">
+
+                      <button class="btn w-full bg-primary-200 py-2 rounded-sm dark:bg-primary-600">
+                        Request A Quote
+                      </button>
+
+                    </div>
+                  </a>
+
                 </div>
-                </a>
-                <a href="/contact">
-                <div class="flex w-full">
-                 
-                  <button class="btn w-full bg-primary-400 py-2 rounded-sm dark:bg-primary-400">
-                    Request A Quote
-                  </button>
-                 
-                </div>
-                </a>
-           
+
               </div>
-
             </div>
-          </div>
 
 
 
 
-          <div class="basis-1/2 pb-16">
-        
-          <img src="https://cdn.svgator.com/images/2021/10/solar-system-animation.svg" width="700" height="400" fetchPriority="high" alt="Animated Vectors Flying to the Moon and Back: About the Space Animation Trend"/>
+            <div class="basis-1/2 pb-16">
+              {/* <Image
+              src={coverImage}
+              layout="constrained"
+              width={500}
+              height={500}
+              alt="Qwind Hero Image (Cools dog)"
+              class="mx-auto lg:mr-0 object-fit w-full drop-shadow-2xl rounded-md"
+              priority={true}
+              breakpoints={[320, 480, 640, 768, 1024]}
+            /> */}
+              <img src="https://cdn.svgator.com/images/2021/10/solar-system-animation.svg" width="700" height="400" fetchPriority="high" alt="Animated Vectors Flying to the Moon and Back: About the Space Animation Trend" />
+            </div>
+
           </div>
 
         </div>
 
-      </div>
-
-    </section>
-  </div>
+      </section>
+    </div>
   );
 });
