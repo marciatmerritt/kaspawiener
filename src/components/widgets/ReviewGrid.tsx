a9499b59e4e4b270e1579d0ec41c2c27cc276080
James <jamesandrewpike@gmail.com>
2025-01-22 09:17:33 -0500

    commit

--- src/components/widgets/ReviewGrid.tsx
+++ src/components/widgets/ReviewGrid.tsx
@@ -4,7 +4,7 @@ export default component$(() => {
   return (
         <div class="grid pt-2 gap-8 lg:grid-cols-3">
             <div class="space-y-6">
-                <figure class="p-6 bg-gray-50 rounded dark:bg-gray-800">
+                <figure class="p-6 bg-gray-100 rounded dark:bg-gray-800">
                     <blockquote class="text-sm text-gray-500 dark:text-gray-400">
                         <h3 class="text-lg font-semibold text-gray-900 dark:text-white">Solid foundation for any project</h3>
                         <p class="my-4">"This is a very complex and beautiful set of elements. Under the hood it comes with the best things from two different worlds: Figma and Tailwind.”</p>
