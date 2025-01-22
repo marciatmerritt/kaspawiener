29a63e68ef312173658293f912019688db05378c
James <jamesandrewpike@gmail.com>
2025-01-22 10:47:51 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -16,7 +16,7 @@ export default component$(() => {
               </a>
               <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
 
-                <span class="text-primary">Premium Digital Art & Restoration</span>
+                <span class="text-primary">Digital Art & Restoration</span>
               </h1>
               <div class="max-w-3xl mx-auto lg:max-w-none">
                 <p class="text-xl mb-6 dark:text-slate-300">
