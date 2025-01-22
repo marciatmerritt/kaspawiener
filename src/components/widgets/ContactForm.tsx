b2cc845cad00ac08b79894b38902916bb3d7f146
James <jamesandrewpike@gmail.com>
2025-01-21 23:28:45 -0500

    commit

--- src/components/widgets/ContactForm.tsx
+++ src/components/widgets/ContactForm.tsx
@@ -1,19 +1,23 @@
 import { component$ } from '@builder.io/qwik';
+import { Headline2 } from '../ui/Headline2';
 
 export default component$(() => {
   return (
  
     <div class="relative isolate bg-white rounded-lg dark:bg-gray-900">
     <div class="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
-        <div class="relative px-6 pb-12 pt-14 sm:pt-32 lg:static lg:px-8 lg:py-24">
+        <div class="relative px-6 pb-12 pt-10 sm:pt-32 lg:static lg:px-8 lg:py-24">
             <div class="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
                 <div class="absolute inset-y-0 left-0 -z-10 w-full overflow-hidden bg-gray-100 ring-1 ring-gray-900/10 dark:bg-gray-900 dark:ring-white/5 lg:w-1/2">
                     <div class="absolute -left-56 top-[calc(100%-13rem)] transform-gpu blur-3xl lg:left-[max(-14rem,calc(100%-59rem))] lg:top-[calc(50%-7rem)]" aria-hidden="true">
                         <div class="aspect-[1155/678] w-[72.1875rem] bg-gradient-to-br from-[#80caff] to-[#4f46e5] opacity-20 dark:opacity-30" style="clip-path: polygon(74.1% 56.1%, 100% 38.6%, 97.5% 73.3%, 85.5% 100%, 80.7% 98.2%, 72.5% 67.7%, 60.2% 37.8%, 52.4% 32.2%, 47.5% 41.9%, 45.2% 65.8%, 27.5% 23.5%, 0.1% 35.4%, 17.9% 0.1%, 27.6% 23.5%, 76.1% 2.6%, 74.1% 56.1%)"></div>
                     </div>
                 </div>
-                <h2 class="text-pretty text-4xl font-semibold tracking-tight text-gray-900 dark:text-white sm:text-5xl">Get in touch</h2>
-                <p class="mt-6 text-lg/8 text-gray-600 dark:text-gray-300">Proin volutpat consequat porttitor cras nullam gravida at. Orci molestie a eu arcu. Sed ut tincidunt integer elementum id sem. Arcu sed malesuada et magna.</p>
+                  <Headline2
+                                title={"Request A Quote"}
+                                subtitle={"Explore how our photography and videography services bring stories to life with creativity and precision."}
+                                highlight={"Contact Us"}
+                              />
                 <dl class="mt-10 space-y-4 text-base/7 text-gray-600 dark:text-gray-300">
                     <div class="flex gap-x-4">
                         <dt class="flex-none">
