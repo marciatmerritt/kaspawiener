1711831fbb51e0172b1b54bbafa4331a3cdbc864
James <jamesandrewpike@gmail.com>
2025-01-22 17:08:26 -0500

    Initialize Repository

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -35,7 +35,9 @@ export default component$(() => {
       <div class="relative text-default py-2.5 px-2.5 md:px-6 mx-auto w-full md:flex md:justify-between max-w-7xl">
         <div class="mr-auto rtl:mr-0 rtl:ml-auto flex justify-between">
           <a class="flex items-center" href={"/"}>
-            <Logo />
+           
+            <img width={50} height={50} src="/images/shield.svg"/>
+            <h1 class="text-2xl pl-1 ">ADEPTLOCK</h1>
           </a>
           <div class="flex items-center md:hidden">
           <a
