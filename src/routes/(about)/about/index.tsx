94e6dc95d6ff5d3b02dd785910bcb4833bfc4b26
James <jamesandrewpike@gmail.com>
2025-01-21 19:32:41 -0500

    commit

--- src/routes/(about)/about/index.tsx
+++ src/routes/(about)/about/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import About from "~/components/pages/About";
-import Contact from "~/components/pages/Contact";
 
 export default component$(() => {
   return (
