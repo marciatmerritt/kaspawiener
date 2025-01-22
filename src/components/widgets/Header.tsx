6e0f6e9b6712582222e0971c7dc8d2228ebb5267
James <jamesandrewpike@gmail.com>
2025-01-22 12:00:50 -0500

    commit

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -105,7 +105,7 @@ export default component$(() => {
                         class="text-gray-500 dark:text-gray-400 hover:bg-primary hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center"
                         aria-label={"Telegram"}
                         title={"Telegram"}
-                        href={"https://t.me/+Zhe2fvsS1WQyNTA0"}
+                        href={"/"}
                       >
                         <IconTelegram />
                       </a>
@@ -114,7 +114,7 @@ export default component$(() => {
                         class="text-gray-500 dark:text-gray-400 mr-2 hover:bg-primary hover:text-white focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 inline-flex items-center"
                         aria-label={"X"}
                         title={"X"}
-                        href={"https://x.com/koinonkas"}
+                        href={"/"}
       
                       >
                         <IconTwitter />
