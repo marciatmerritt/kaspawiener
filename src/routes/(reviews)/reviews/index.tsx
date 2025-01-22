1c7e120b498df5506a89d54388264f8bfbb7c901
James <jamesandrewpike@gmail.com>
2025-01-22 09:15:15 -0500

    commit

--- src/routes/(reviews)/reviews/index.tsx
+++ src/routes/(reviews)/reviews/index.tsx
@@ -1,6 +1,5 @@
 import { component$ } from "@builder.io/qwik";
 import Reviews from "~/components/pages/Reviews";
-import ReviewGrid from "~/components/widgets/ReviewGrid";
 
 export default component$(() => {
   return (
