11bde3cd3da14f4d647e9149cc72aefd3c2d065a
James <jamesandrewpike@gmail.com>
2025-01-23 11:11:03 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,16 +4,15 @@ import Contact from "~/components/pages/Contact";
 import Hero from "~/components/widgets/Hero";
 import Steps from "~/components/widgets/Steps";
 import { SITE } from "~/config.mjs";
-import { qwikSerialized } from "~/utils/qwikSerialized";
 import Services from "~/components/pages/Services";
 import Reviews from "~/components/pages/Reviews";
 import FAQmobile from "~/components/pages/FAQmobile";
 
-const IconBrandTailwind = qwikSerialized(() => import("../components/icons/IconBrandTailwind"));
-const IconApps = qwikSerialized(() => import("../components/icons/IconApps"));
-const IconRocket = qwikSerialized(() => import("../components/icons/IconRocket"));
-const IconBrandGoogle = qwikSerialized(() => import("../components/icons/IconBrandGoogle"));
-const IconBulb = qwikSerialized(() => import("../components/icons/IconBulb"));
+// const IconBrandTailwind = qwikSerialized(() => import("../components/icons/IconBrandTailwind"));
+// const IconApps = qwikSerialized(() => import("../components/icons/IconApps"));
+// const IconRocket = qwikSerialized(() => import("../components/icons/IconRocket"));
+// const IconBrandGoogle = qwikSerialized(() => import("../components/icons/IconBrandGoogle"));
+// const IconBulb = qwikSerialized(() => import("../components/icons/IconBulb"));
 
 export default component$(() => {
   return (
