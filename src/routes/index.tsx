387ac909a8798e526c09adaac7ab8243d4575264
James <jamesandrewpike@gmail.com>
2025-01-23 08:52:26 -0500

    Initialize Repository

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -9,6 +9,7 @@ import { qwikSerialized } from "~/utils/qwikSerialized";
 import Services from "~/components/pages/Services";
 import Reviews from "~/components/pages/Reviews";
 import Pricing from "~/components/pages/Pricing";
+import ReviewCarousel2 from "~/components/widgets/ReviewCarousel2";
 
 const IconBrandTailwind = qwikSerialized(() => import("../components/icons/IconBrandTailwind"));
 const IconApps = qwikSerialized(() => import("../components/icons/IconApps"));
@@ -20,6 +21,7 @@ export default component$(() => {
   return (
     <>
       <Hero />
+      <ReviewCarousel2/>
       <Services/>
       <Steps/>
       <div class="sm:hidden">
