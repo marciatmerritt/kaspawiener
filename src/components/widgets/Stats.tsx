97645480f43679c86e969d800c5ac36fcf62a3dc
James <jamesandrewpike@gmail.com>
2025-01-23 18:13:22 -0500

    commit

--- src/components/widgets/Stats.tsx
+++ src/components/widgets/Stats.tsx
@@ -2,7 +2,7 @@ import { component$ } from "@builder.io/qwik";
 
 export default component$(() => {
   return (
-    <div id="tokenomics" class="px-4 pt-3 pb-6 md:py-16 sm:px-6 mx-auto md:px-24 lg:px-8  lg:pb-20 lg:py-6 max-w-7xl">
+    <div id="tokenomics" class="px-4 pt-3 pb-6 md:py-16 sm:px-6 mx-auto md:px-24 lg:px-8  lg:pb-20 lg:py-4 max-w-7xl">
       <div class="grid grid-cols-2 row-gap-8 md:grid-cols-4">
         <div class="text-center md:border-r dark:md:border-slate-500 mb-10 md:mb-0">
           <div class="text-4xl font-bold lg:text-5xl xl:text-6xl text-primary-600 font-heading">
