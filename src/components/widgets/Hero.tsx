1ac0b7500fd4e40b50b7306b472b39db9e83d1d5
James <jamesandrewpike@gmail.com>
2025-01-23 18:07:23 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -21,7 +21,7 @@ export default component$(() => {
               </h1>
               <div class="max-w-3xl mx-auto lg:max-w-none">
                 <p class="text-xl mb-6 dark:text-slate-300">
-                  Transform your online presence with our custom web design and development services. We help business create stunning, fast, and secure websites.
+                  Transform your online presence with our custom web design and development services.
                 </p>
 
                 <div class=" sm:max-w-md m-auto grid grid-cols-1 border-radius-dot-25 gap-4 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
