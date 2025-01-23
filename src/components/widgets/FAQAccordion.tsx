488b3253786a0738f661ef445d17c759d91ab1b5
James <jamesandrewpike@gmail.com>
2025-01-23 10:37:13 -0500

    Initialize Repository

--- src/components/widgets/FAQAccordion.tsx
+++ src/components/widgets/FAQAccordion.tsx
@@ -7,12 +7,12 @@ export default component$(() => {
       {/* Services Section */}
       <Accordion.Item class="dark:border-gray-700">
         <Accordion.Trigger class="text-xl flex trigger-text justify-between hover:text-primary items-center py-3 border-b border-gray-300 dark:border-gray-700">
-          <span>Story</span>
+          <span>How many questions are in this FAQ accordion?</span>
         </Accordion.Trigger>
         <Accordion.Content class="">
           {/* Service Links with Highlight and Divider */}
           <div class="hover:bg-primary/35 transition border-b border-gray-300 dark:border-gray-700 last:border-none">
-            <a href="/" class="block text-lg py-3 px-2">Web Design</a>
+            <a href="/" class="block text-lg py-3 px-2">There are 5 questions and answers in this FAQ accordion.</a>
           </div>
     
         </Accordion.Content>
