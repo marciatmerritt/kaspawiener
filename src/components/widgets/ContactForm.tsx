b3ede34100eccb91118ec136e1d29b1788cc772f
James <jamesandrewpike@gmail.com>
2025-01-22 20:42:30 -0500

    Initialize Repository

--- src/components/widgets/ContactForm.tsx
+++ src/components/widgets/ContactForm.tsx
@@ -94,7 +94,7 @@ export default component$(() => {
                         </div>
                     </div>
                 </div>
-                <div class="mt-3 flex">
+                <div class="mt-3.5 flex">
                 <button type="submit" class="py-3 w-full px-5 text-sm font-medium text-center text-white rounded-md bg-primary-700 sm:w-fit hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Send message</button>
                 </div>
             </div>
