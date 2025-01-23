85903bc3410d894c9401cbc6b0ea4d84e9bd2556
James <jamesandrewpike@gmail.com>
2025-01-23 13:27:53 -0500

    commit

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,11 +2,8 @@ import { component$ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 import Contact from "~/components/pages/Contact";
 import Hero from "~/components/widgets/Hero";
-import Steps from "~/components/widgets/Steps";
 import { SITE } from "~/config.mjs";
-import Services from "~/components/pages/Services";
-import Reviews from "~/components/pages/Reviews";
-import FAQmobile from "~/components/pages/FAQmobile";
+
 
 // const IconBrandTailwind = qwikSerialized(() => import("../components/icons/IconBrandTailwind"));
 // const IconApps = qwikSerialized(() => import("../components/icons/IconApps"));
