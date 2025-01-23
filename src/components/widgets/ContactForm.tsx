71b45173756b7954a9ee80b8940124e112901801
James <jamesandrewpike@gmail.com>
2025-01-23 11:21:51 -0500

    commit

--- src/components/widgets/ContactForm.tsx
+++ src/components/widgets/ContactForm.tsx
@@ -49,7 +49,7 @@ export default component$(() => {
                 </dl>
             </div>
         </div>
-        <form action="#" method="POST" class="px-4 pb-6 pt-8 sm:pb-32 lg:px-8 lg:py-24">
+        <form action="#" method="POST" class="px-6 pb-6 pt-8 sm:pb-32 lg:px-8 lg:py-24">
             <div class="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
                 <div class="grid grid-cols-1 gap-x-8 gap-y-4 sm:grid-cols-2">
                     <div>
