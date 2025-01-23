fbf03f543df937ad2e439179b1d429e8ecf658dc
James <jamesandrewpike@gmail.com>
2025-01-23 17:53:47 -0500

    commit

--- src/components/common/PageWrapper.tsx
+++ src/components/common/PageWrapper.tsx
@@ -5,7 +5,7 @@ export const PageWrapper = component$(() => {
   return (
     <div class="relative bg-white/80 dark:bg-gray-900/80 overflow-hidden flex items-center justify-center mx-1.5 mt-1 mb-1.5 border-radius-dot-25 rounded-lg">
       <div class="relative w-full max-w-7xl mx-auto px-1.5 py-1.5 lg:px-8">
-        <div class="w-full bg-white/85 sm:bg-white/90 dark:bg-slate-900/85 rounded-lg pt-6 p-3 border-radius-dot-25">
+        <div class="w-full bg-white/85 sm:bg-white/90 dark:bg-slate-900/85 rounded-lg pt-8 p-3 border-radius-dot-25">
           <Slot />
         </div>
       </div>
