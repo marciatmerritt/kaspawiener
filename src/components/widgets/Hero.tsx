01ffd3ed01cd303785a6218b28c44564ed7ee0ba
James <jamesandrewpike@gmail.com>
2025-01-22 20:36:45 -0500

    Initialize Repository

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -16,11 +16,11 @@ export default component$(() => {
               </a>
               <h1 class="text-5xl md:text-7xl font-bold leading-tighter tracking-tighter text-primary mb-4 font-heading dark:text-gray-200">
 
-                <span class="text-primary">Digital Art & Restoration</span>
+                <span class="text-primary">Premium Design & Development Services</span>
               </h1>
               <div class="max-w-3xl mx-auto lg:max-w-none">
                 <p class="text-xl mb-6 dark:text-slate-300">
-                Expertly restoring photos, creating custom digital art, and delivering premium prints to preserve your most treasured moments.
+                  Transform your online presence with our custom web design and development services. We help business create stunning, fast, and secure websites.
                 </p>
 
                 <div class=" sm:max-w-md m-auto grid grid-cols-1 border-radius-dot-25 gap-4 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
