d031e3cff8362066392dd050125fe34becff526c
James <jamesandrewpike@gmail.com>
2025-01-23 20:47:30 -0500

    commit

--- src/components/pages/Whitepaper.tsx
+++ src/components/pages/Whitepaper.tsx
@@ -3,8 +3,12 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <div class="w-screen overflow-hidden">
-    <embed src="/images/whitepaper.pdf" type="application/pdf" class="w-full h-screen" />
-
+      <iframe
+        src="/images/whitepaper.pdf"
+        class="w-full h-screen md:h-[90vh]"
+        style="border: none;"
+        title="Whitepaper"
+      ></iframe>
     </div>
   );
 });
