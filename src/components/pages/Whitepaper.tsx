3cc58fec68706850ddb00f6b086e4580964329a2
James <jamesandrewpike@gmail.com>
2025-01-23 20:06:33 -0500

    commit

--- src/components/pages/Whitepaper.tsx
+++ src/components/pages/Whitepaper.tsx
@@ -3,11 +3,14 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
   return (
     <div class="w-screen overflow-hidden">
-   <iframe
-  src="/images/whitepaper.pdf"
-  style="width: 100vw; max-width: 100%; height: 100vh; border: none;"
-  title="Whitepaper"
-></iframe>
+      <div class="relative w-full aspect-[16/9]">
+        <iframe
+          src="/images/whitepaper.pdf"
+          class="absolute top-0 left-0 w-full h-full"
+          style="border: none;"
+          title="Whitepaper"
+        ></iframe>
+      </div>
     </div>
   );
 });
