1cd5213cb6763ce4758ad05c4bbd3c0c882922bb
James <jamesandrewpike@gmail.com>
2025-01-24 14:40:24 -0500

    commit

--- src/components/widgets/Hero.tsx
+++ src/components/widgets/Hero.tsx
@@ -21,7 +21,7 @@ export default component$(() => {
               </h1>
               <div class="max-w-3xl mx-auto lg:max-w-none">
                 <p class="text-xl mb-6 dark:text-slate-300">
-                Join the fun with WIENER— the top dog on the Kaspa chain. Fast, fun, and ready for the next big trend.                </p>
+                Join the fun with $WIENER— the top dog on the Kaspa chain. Fast, fun, and ready for the next big trend.                </p>
 
                 <div class=" sm:max-w-md m-auto grid grid-cols-2 border-radius-dot-25 gap-3 sm:grid-cols-2 lg:grid-cols-2 lg:max-w-7xl">
                   <a href="https://x.com/wienerKRC20">
