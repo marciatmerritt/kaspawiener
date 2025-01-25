504b9df655dfe601c58f4731ed29d5d332cd1ffb
James <jamesandrewpike@gmail.com>
2025-01-24 19:32:58 -0500

    commit

--- src/components/widgets/Steps.tsx
+++ src/components/widgets/Steps.tsx
@@ -38,7 +38,7 @@ export default component$(() => {
   };
   
   
-  const { title, items, image } = stepsData;
+  const { title, items} = stepsData;
 
   return (
     <section class="mx-auto max-w-6xl px-4 py-16 sm:px-6 lg:px-8 lg:py-20 bg-gray-100 dark:bg-gray-800">
