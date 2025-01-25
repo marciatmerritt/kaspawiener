5d46f14de700d96eba4336bff2087fb44b1c79c4
Tester <test@email.com>
2025-01-25 16:23:17 -0500

    commit

--- src/components/widgets/Header.tsx
+++ src/components/widgets/Header.tsx
@@ -36,7 +36,7 @@ export default component$(() => {
       <div class="relative text-default py-2.5 px-2.5 md:px-6 mx-auto w-full md:flex md:justify-between max-w-7xl">
         <div class="mr-auto rtl:mr-0 rtl:ml-auto flex justify-between">
           <a class="flex items-center" href={"/"}>
-          <img class="rounded-full" src="/images/logo.jpg" height={50} width={50}></img>
+          <img class="rounded-full" src="/images/logo.svg" height={50} width={50}></img>
           <span class="self-center ml-1 sm:ml-2 text-3xl md:text-2xl font-bold text-primary hover:text-primary-300 whitespace-nowrap flex items-center">
   
   WIENER
--- src/routes/(trade)/trade/index.tsx
+++ src/routes/(trade)/trade/index.tsx
@@ -1,5 +1,4 @@
 import { component$ } from "@builder.io/qwik";
-import Story from "~/components/pages/Story";
 import Trade from "~/components/pages/Trade";
 
 export default component$(() => {
