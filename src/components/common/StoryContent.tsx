1fe944fbf4db3adf6a381c7af692e1b703cc5655
Marcia Merritt <marciatmerritt@gmail.com>
2025-03-14 16:21:30 -0400

    chore: refactor Story Page to use JSON-based data, Qwik image optimization, and reusable StoryContent component

---
+++ src/components/common/StoryContent.tsx
@@ -0,0 +1,67 @@
+import { component$ } from '@builder.io/qwik';
+import { Headline2 } from '../ui/Headline2';
+import { Headline } from '../ui/Headline';
+import { Image } from '@unpic/qwik';
+import storyContent from '../../utils/storyContent.json';
+
+export const StoryContent = component$(
+  ({
+    PageWrapperComponent,
+    storyId,
+  }: {
+    PageWrapperComponent: any;
+    storyId?: string;
+  }) => {
+    let stories = storyContent.stories;
+    if (storyId) {
+      // Filter for a single story if `storyId` is provided
+      stories = stories.filter((story) => story.id === storyId);
+    }
+
+    return (
+      <PageWrapperComponent>
+        {stories.map((story) => {
+          const { title, subtitle, highlight, image } = story;
+          const localSrc = `/images/${image.fileName}`;
+          const imageSrc = localSrc
+          return (
+            <div
+              key={story.id}
+              class='flex flex-col items-center gap-6 p-4 md:flex-row md:items-start md:justify-between md:gap-8 md:p-8'
+            >
+              {/* Text Container */}
+              <div class='order-1 flex w-full flex-col justify-center md:order-2 md:w-1/2'>
+                <div class='hidden sm:block'>
+                  <Headline2
+                    title={title}
+                    subtitle={subtitle}
+                    highlight={highlight}
+                  />
+                </div>
+                <div class='block sm:hidden'>
+                  <Headline
+                    title={title}
+                    subtitle={subtitle}
+                    highlight={highlight}
+                  />
+                </div>
+              </div>
+
+              {/* Image Container */}
+              <div class='order-2 w-full md:order-1 md:w-1/2'>
+                <Image
+                  src={imageSrc}
+                  alt={image.alt}
+                  width={image.width}
+                  height={image.height}
+                  layout="constrained"
+                  class='h-auto w-full rounded-lg shadow-md'
+                />
+              </div>
+            </div>
+          );
+        })}
+      </PageWrapperComponent>
+    );
+  }
+);
--- src/components/pages/Story.tsx
+++ src/components/pages/Story.tsx
@@ -1,45 +1,7 @@
 import { component$ } from '@builder.io/qwik';
-import { Headline2 } from '../ui/Headline2';
 import { PageWrapper } from '../common/PageWrapper';
-import { Headline } from '../ui/Headline';
+import { StoryContent } from '../common/StoryContent';
 
 export default component$(() => {
-  return (
-    <PageWrapper>
-      <div class='flex flex-col items-center gap-6 p-4 md:flex-row md:items-start md:justify-between md:gap-8 md:p-8'>
-        {/* Text Container */}
-        <div class='order-1 flex w-full flex-col justify-center md:order-2 md:w-1/2'>
-          <div class='hidden sm:block'>
-            <Headline2
-              title={'Bailey the $WIENER on Kaspa'}
-              subtitle={
-                'Bailey is the first $WIENER and the origin story. The genesis. Many other wieners will come after Bailey but she started this rocket ship for all the other dogs.'
-              }
-              highlight={'Story'}
-            />
-          </div>
-          <div class='block sm:hidden'>
-            <Headline
-              title={'Bailey the $WIENER'}
-              subtitle={
-                'Bailey is the first $WIENER and the origin story. The genesis. Many other wieners will come after Bailey but she started this rocket ship for all the other dogs.'
-              }
-              highlight={'Story'}
-            />
-          </div>
-        </div>
-
-        {/* Image Container */}
-        <div class='order-2 w-full md:order-1 md:w-1/2'>
-          <img
-            height={767}
-            width={767}
-            src='/images/bailey2.jpg'
-            alt='Sample Image'
-            class='h-auto w-full rounded-lg shadow-md'
-          />
-        </div>
-      </div>
-    </PageWrapper>
-  );
+  return <StoryContent PageWrapperComponent={PageWrapper} />;
 });
