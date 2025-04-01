1f7a839417ae6e981696e5296d92c2e61acded7c
Marcia Merritt <marciatmerritt@gmail.com>
2025-03-31 21:00:55 -0400

    fix(trade): use native <img> for consistent image sizing on small screens

--- src/components/common/TradeContent.tsx
+++ src/components/common/TradeContent.tsx
@@ -1,6 +1,5 @@
 import { component$ } from '@builder.io/qwik';
 import { Headline } from '../ui/Headline';
-import { Image } from '@unpic/qwik';
 import { tradeData } from '../../data/trade';
 import { getImageSrc } from '../../utils/ui';
 import { Anchor } from '../ui/Anchor';
@@ -10,6 +9,13 @@ import { Anchor } from '../ui/Anchor';
  *
  * Displays a section with external trading platform links and images.
  * Includes a headline and a responsive grid of logo cards that link to supported exchanges or tools.
+ *
+ * We're using regular <img> tags instead of the Unpic <Image> component here.
+ * Why? Because <img> tags work better when we want all images to look the same size,
+ * especially when the original images are different shapes (like square vs. wide).
+ *
+ * By setting the width and height on the <img> tag and using Tailwind's h-48 and object-cover,
+ * the images get cropped and sized consistently across all screen sizes.
  *
  * @component
  * @param {{ PageWrapperComponent: any }} props - Layout wrapper component used to apply consistent page structure.
@@ -31,12 +37,11 @@ export const TradeContent = component$(
           {images.map((image) => {
             return (
               <Anchor key={image.id} href={image.link} target='_blank'>
-                <Image
+                <img
                   src={getImageSrc(image)}
                   alt={image.alt}
                   width={image.width}
                   height={image.height}
-                  layout={image.layout}
                   class='h-48 w-full rounded-md object-cover hover:opacity-80'
                 />
               </Anchor>
